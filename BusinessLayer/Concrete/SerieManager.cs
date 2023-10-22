e1d7139f08a5ae8e5a0c9d18763ead5a2b3e8864
Ibrahim <ibrahimnet2002@hotmail.com>
2023-10-22 11:48:30 +0300

    Dizi Controller ve service dosyaları oluşturuldu. Databaseden dizi çekme işlemi yapıldı

---
+++ BusinessLayer/Concrete/SerieManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class SerieManager : ISerieService
+    {
+        ISerie _serie;
+
+        public SerieManager(ISerie serie)
+        {
+            _serie = serie;
+        }
+
+        public void SerieAdd(Serie serie)
+        {
+            _serie.Add(serie);
+        }
+
+        public void SerieRemove(Serie serie)
+        {
+            _serie.Delete(serie);
+        }
+
+        public void SerieUpdate(Serie serie)
+        {
+            _serie.Update(serie);
+        }
+
+        public Serie GetById(int id)
+        {
+            return _serie.GetById(id);
+        }
+
+        public List<Serie> ListAll()
+        {
+            return _serie.ListAll();
+        }
+    }
+}
---
+++ WebApplication5/Controllers/SerieController.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.Controllers
+{
+    public class SerieController : Controller
+    {
+        SerieManager cm = new SerieManager(new EFSerieRepository());
+        public IActionResult Index()
+        {
+            var values = cm.ListAll();
+            return View(values);
+        }
+    }
+}
