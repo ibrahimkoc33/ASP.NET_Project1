8dde995a16ffcd6ac9350074fe005663abf17ca4
Ibrahim <ibrahimnet2002@hotmail.com>
2023-11-08 21:17:50 +0300

    .

--- WebApplication5/Controllers/MovieController.cs
+++ WebApplication5/Controllers/MovieController.cs
@@ -14,19 +14,37 @@ namespace WebApplication5.Controllers
 		}
 		public IActionResult MovieDetails(int id)
 		{
-			ViewBag.i=id;
-			var values= mm.GetMovieById(id);
+			ViewBag.i = id;
+			var values = mm.GetMovieById(id);
 			return View(values);
 		}
 		public IActionResult MovieList()
 		{
-			var values= mm.GetMovieByCategory();
+			var values = mm.GetMovieByCategory();
 			return View(values);
 		}
 		public IActionResult MovieListByGrid()
 		{
 			var values = mm.GetMovieByCategory();
 			return View(values);
+		}
+
+		[HttpGet("api/movies")]
+		public IActionResult GetMovies()
+		{
+			var movies = mm.GetMovieByCategory();
+			return Ok(movies);
+		}
+
+		[HttpGet("api/movies/{id}")]
+		public IActionResult GetMovie(int id)
+		{
+			var movie = mm.GetMovieById(id);
+			if (movie == null)
+			{
+				return NotFound();
+			}
+			return Ok(movie);
 		}
 	}
 }
