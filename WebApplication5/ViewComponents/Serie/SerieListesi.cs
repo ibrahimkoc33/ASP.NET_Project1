7097fa3ae132b57b63a9f54abecb9d3d98bd6984
Ibrahim <ibrahimnet2002@hotmail.com>
2023-11-13 15:07:10 +0300

    Anasayfaya Diziler eklendi

---
+++ WebApplication5/ViewComponents/Serie/SerieListesi.cs
@@ -0,0 +1,16 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication5.ViewComponents.Serie
+{
+	public class SerieListesi:ViewComponent
+	{
+		SerieManager rm = new SerieManager(new EFSerieRepository());
+		public IViewComponentResult Invoke()
+		{
+			var values = rm.ListAll();
+			return View(values);
+		}
+	}
+}
