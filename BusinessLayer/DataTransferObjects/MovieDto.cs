c111b7825ba7a36a3008350368ddedcd97ae9ec1
Ibrahim <ibrahimnet2002@hotmail.com>
2024-01-05 19:29:51 +0300

    .

---
+++ BusinessLayer/DataTransferObjects/MovieDto.cs
@@ -0,0 +1,42 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.DataTransferObjects
+{
+    public class MovieDto
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Genre { get; set; }
+
+        public string Director { get; set; } //list olabilir
+
+        public string Scenarist { get; set; }
+
+        public string Cast { get; set; } // liste olucak düzelt
+
+        public string Image { get; set; }
+
+        public float Star { get; set; }
+
+        public int RunTime { get; set; }
+
+        public string Overview { get; set; }
+
+        public int Date { get; set; }
+
+        public List<Rewiew>? Rewiews { get; set; }
+
+        public int? CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        public int[]? CategoryIds { get; set; }
+    }
+}
---
+++ DataAccessLayer/Migrations/20240105162932_m31.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class m31 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Categorys_CategoryId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Movies",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Categorys_CategoryId",
+                table: "Movies",
+                column: "CategoryId",
+                principalTable: "Categorys",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Categorys_CategoryId",
+                table: "Movies");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Movies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Categorys_CategoryId",
+                table: "Movies",
+                column: "CategoryId",
+                principalTable: "Categorys",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
