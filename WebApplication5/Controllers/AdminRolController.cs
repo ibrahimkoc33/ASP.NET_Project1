edf4da87c3ee79280da6fe70eea1796f217f9dfc
Ibrahim <ibrahimnet2002@hotmail.com>
2024-01-06 19:52:52 +0300

    Rol Ekleme

--- WebApplication5/Controllers/AdminRolController.cs
+++ WebApplication5/Controllers/AdminRolController.cs
@@ -1,12 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication5.Models;
 
 namespace WebApplication5.Controllers
 {
 	public class AdminRolController : Controller
 	{
-		public IActionResult Index()
+		private readonly RoleManager<AppRole> _roleMenager;
+
+        public AdminRolController(RoleManager<AppRole> roleMenager)
+        {
+            _roleMenager = roleMenager;
+        }
+
+        public IActionResult Index()
+		{
+			var values = _roleMenager.Roles.ToList();
+			return View(values);
+		}
+		[HttpGet]
+		public IActionResult AddRole() 
 		{
 			return View();
 		}
-	}
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+			if(ModelState.IsValid)
+			{
+				AppRole role = new AppRole
+				{
+					Name = model.name
+					
+				};
+				var result = await _roleMenager.CreateAsync(role);
+				if(result.Succeeded)
+				{
+					return RedirectToAction("Index","AdminRol");
+				}
+				foreach(var item in result.Errors)
+				{
+					ModelState.AddModelError("", item.Description);
+				}
+			}
+            return View(model);
+        }
+    }
 }
---
+++ WebApplication5/Models/RoleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication5.Models
+{
+    public class RoleViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Rol Adı giriniz")]
+        public string name { get; set; }
+    }
+}
